08133f5917c3176014720c8c1b32217fa23f1895
Ryan Leto <ryjleto@gmail.com>
2015-08-30 07:05:07 +0000

    added model and style changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   root :to => 'static_pages#home'
-  get '/help' => 'static_pages#help'
+  get '/home' => 'static_pages#home'
   get '/about' => 'static_pages#about'
   get '/contact' => 'static_pages#contact'
   
