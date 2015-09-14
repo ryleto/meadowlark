161d0b0988be0f21eb28eeee4c44906ae287798c
Ryan Leto <ryjleto@gmail.com>
2015-09-14 05:01:38 +0000

    running additional integration tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -9,6 +9,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password: "wrong",
                                password_confirmation: "wrongagain" }
     end
-    assert_template 'users/new'                           
+    assert_template 'users/new'
+  end
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name: "Flavia Pennetta",
+                                            email: "user@example.com",
+                                            password: "password",
+                                            password_confirmation: "password" }
+    end
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
