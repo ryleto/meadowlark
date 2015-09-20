5a3dd18b5ec8f016bf26c44aff752ee9b95823f8
Ryan Leto <ryjleto@gmail.com>
2015-09-20 16:42:15 +0000

    extending user integration tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -38,4 +40,13 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
     
+    # before filters
+    
+    # confirms user is logged in
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = 'You must log in first to continue'
+        redirect_to login_url
+      end
+    end
 end
