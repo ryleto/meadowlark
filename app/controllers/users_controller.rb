50b6731209986b99856857d6dc6ecc7490681868
Ryan Leto <ryjleto@gmail.com>
2015-09-20 19:26:48 +0000

    modifying/testing sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -45,8 +46,15 @@ class UsersController < ApplicationController
     # confirms user is logged in
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = 'You must log in first to continue'
         redirect_to login_url
       end
+    end
+    
+    # confirms current user is correct
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -28,4 +28,21 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal @user.name, name
     assert_equal @user.email, email
   end
+  
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
+    name  = "Thom Yorke"
+    email = "tyorke@example.com"
+    patch user_path(@user), user: { name:  name,
+                                    email: email,
+                                    password:              "",
+                                    password_confirmation: "" }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
 end
