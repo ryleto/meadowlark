575f03be24b5f5c1c4e8016c982cb24314571c93
Ryan Leto <ryjleto@gmail.com>
2015-09-21 00:46:06 +0000

    changes to user auth component

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@
 User.create!(name:  "Example User",
              email: "example@example.com",
              password:              "password",
-             password_confirmation: "password")
+             password_confirmation: "password",
+             admin: true)
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@example.com"
