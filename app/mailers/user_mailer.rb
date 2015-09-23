f5428fed35fc01c3df7b0a58bdc9be2c5b50fb32
Ryan Leto <ryjleto@gmail.com>
2015-09-23 13:34:16 +0000

    user mailer changes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.account_activation.subject
   #
-  def account_activation
+  def account_activation(user)
     @user = user
     mail to: user.email, subject: "Account activation"
   end
@@ -15,7 +15,7 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset
+  def password_reset(user)
     @greeting = "Hi"
 
     mail to: "to@example.org"
