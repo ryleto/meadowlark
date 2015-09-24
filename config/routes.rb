2056009c3cc16da31b2308e5038709a2182fbeff
Ryan Leto <ryjleto@gmail.com>
2015-09-24 04:48:30 +0000

    generating new controllers and migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   # static page routes
@@ -12,6 +16,7 @@ Rails.application.routes.draw do
   # user routes
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921051418) do
+ActiveRecord::Schema.define(version: 20150924044217) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",              limit: 255
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20150921051418) do
     t.string   "activation_digest", limit: 255
     t.boolean  "activated",         limit: 1,   default: false
     t.datetime "activated_at"
+    t.string   "reset_digest",      limit: 255
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
