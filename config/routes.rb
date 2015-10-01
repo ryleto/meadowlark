50c2aff9491c8b1261e50e2b6d757b19effdd7e5
Ryan Leto <ryjleto@gmail.com>
2015-10-01 02:28:58 +0000

    cleaning up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,22 @@
 Rails.application.routes.draw do
 
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-  get 'sessions/new'
-
   # static page routes
-  root :to => 'static_pages#home'
-  get '/home' => 'static_pages#home'
-  get '/about' => 'static_pages#about'
-  get '/contact' => 'static_pages#contact'
-  get '/news' => 'static_pages#news'
+  root                'static_pages#home'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'news'    => 'static_pages#news'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  
+  get 'password_resets/new'
+  get 'password_resets/edit'
+  get 'sessions/new'
   
   # user routes
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
-  get '/signup' => 'users#new'
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  delete '/logout' => 'sessions#destroy'
  
 end
