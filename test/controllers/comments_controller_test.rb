2f24184f928f21ab31384adb048e067ac8a4e138
Ryan Leto <ryjleto@gmail.com>
2015-10-01 03:39:15 +0000

    controller updates

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @comment = comments(:one)
+  end
+  
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Comment.count' do
+      delete :destroy, id: @comment
+    end
+    assert_redirected_to login_url
+  end
+  
 end
