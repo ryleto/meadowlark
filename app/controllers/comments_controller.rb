273204437cb91be3b66862fce9ba4cd18331cb12
Ryan Leto <ryjleto@gmail.com>
2015-10-02 21:24:54 +0000

    controller updates

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy]
+    before_action :correct_user,   only: :destroy
     
     def create
         @comment = current_user.comments.build(comment_params)
@@ -13,11 +14,19 @@ class CommentsController < ApplicationController
     end
 
     def destroy
+        @comment.destroy
+        flash[:success] = "Comment deleted"
+        redirect_to request.referrer || root_url
     end
     
     private
 
         def comment_params
           params.require(:comment).permit(:content)
+        end
+        
+        def correct_user
+          @comment = current_user.comments.find_by(id: params[:id])
+          redirect_to root_url if @comment.nil?
         end
 end
