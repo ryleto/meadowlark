53640342abf11454952eb78955af3ae829da12e4
Ryan Leto <ryjleto@gmail.com>
2015-10-03 19:48:19 +0000

    new unit test

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -12,4 +12,12 @@ class CommentsControllerTest < ActionController::TestCase
     assert_redirected_to login_url
   end
   
+  test "should redirect destroy for wrong comment" do
+    log_in_as(users(:rjleto))
+    comment = comments(:one)
+    assert_no_difference 'Comment.count' do
+      delete :destroy, id: comment
+    end
+    assert_redirected_to root_url
+  end
 end
