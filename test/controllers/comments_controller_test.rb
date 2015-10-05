c4ec2ca0b4123b9d286b86e71317043cbb4c43aa
Ryan Leto <ryjleto@gmail.com>
2015-10-05 05:35:46 +0000

    controller tests

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -7,6 +7,13 @@ class CommentsControllerTest < ActionController::TestCase
   
   test "should redirect create when not logged in" do
     assert_no_difference 'Comment.count' do
+      post :create, comment: { content: "another comment" }
+    end
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect destory when not logged in" do
+    assert_no_difference 'Comment.count' do
       delete :destroy, id: @comment
     end
     assert_redirected_to login_url
@@ -14,7 +21,7 @@ class CommentsControllerTest < ActionController::TestCase
   
   test "should redirect destroy for wrong comment" do
     log_in_as(users(:rjleto))
-    comment = comments(:one)
+    comment = comments(:two)
     assert_no_difference 'Comment.count' do
       delete :destroy, id: comment
     end
