d39e1e23844edc2e06cf1dedc4d4b61d770d9a31
Ryan Leto <ryjleto@gmail.com>
2015-10-09 05:06:31 +0000

    view/controller updates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-    before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+    before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
     
     def index
         @articles = Article.paginate(page: params[:page])
