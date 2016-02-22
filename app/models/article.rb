de26bb6f9b3575fab177eb4fad68bc557861f68f
Ryan Leto <ryjleto@gmail.com>
2016-02-22 04:59:45 +0000

    validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ActiveRecord::Base
     default_scope -> { order(pubdate: :desc) }
+    validates :pubdate, :title, :source, :presence => true
+    validates :title, :source, :uniqueness => true
 end
