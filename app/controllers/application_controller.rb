b6245251c3e65f0118a96be90079338e1697dc74
Ryan Leto <ryjleto@gmail.com>
2016-05-26 14:57:02 +0000

    controller updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,7 @@ class ApplicationController < ActionController::Base
     def logged_in_user
       unless logged_in?
         store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
+        redirect_to login_url, alert: "Please log in."
       end
     end
 end
