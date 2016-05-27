f92328127d1384bce948d30134d25ba0fa74be2a
Ryan Leto <ryjleto@gmail.com>
2016-05-27 22:01:49 +0000

    controller updates

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -5,11 +5,9 @@ class AccountActivationsController < ApplicationController
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user 
-      flash[:success] = "Account activated!"
-      redirect_to user
+      redirect_to user, notice: "Account activated!"
     else
-      flash[:danger] = "Invalid activation link"
-      redirect_to root_url
+      redirect_to root_url, alert: "Invalid activation link"
     end
   end
   
--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,8 +11,7 @@ class PasswordResetsController < ApplicationController
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      flash[:info] = "Email sent with password reset instructions"
-      redirect_to root_url
+      redirect_to root_url, notice: "Email sent with password reset instructions"
     else
       flash.now[:danger] = "Email address not found"
       render 'new'
